0d8689c68ce816a35582c0b751cb93a0db67706c
Brit Butler <redline6561@gmail.com>
2016-03-15 11:36:55 -0400

    Add basic search API to jukeborx.

--- lib/jukeborx.rb
+++ lib/jukeborx.rb
@@ -3,11 +3,29 @@ require "pry"
 
 require "jukeborx/version"
 
+MUSIC_DIR = "/Users/brit/Music/downloads"
+
 module Jukeborx
   class Api < Sinatra::Application
+    set :library, Library.new(MUSIC_DIR)
 
     get "/test" do
       "The API is up!"
+    end
+
+    ## Params:
+    ##  type (required) - One of 'artist', 'album', 'title'
+    ##  value - "radiohead", "supermodified", "bad blood"
+    get "/api/search" do
+      if params["type"] == "artist"
+        settings.library.match_artists(params["value"])
+      elsif params["type"] == "album"
+        settings.library.match_albums(params["value"])
+      elsif params["type"] == "title"
+        settings.library.match_titles(params["value"])
+      else
+        { error: "Invalid search type." }.to_json
+      end
     end
 
     run! if app_file == $0
