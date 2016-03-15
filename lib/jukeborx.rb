c46f6996c8f03c043d9d3f32394778b2e6ada768
Brit Butler <redline6561@gmail.com>
2016-03-15 09:28:58 -0400

    Some initial sinatra experiments.

--- lib/jukeborx.rb
+++ lib/jukeborx.rb
@@ -1,5 +1,38 @@
+require "sinatra"
+require "pry"
+
 require "jukeborx/version"
 
 module Jukeborx
   # Your code goes here...
+end
+
+class Student
+end
+
+get '/hi' do
+  Student.new.to_s
+end
+
+
+get "/add-one" do
+  @count ||= 0
+  @count += 1
+  "\n\n The count is #{@count}! \n\n"
+end
+
+get "/repos/:org/:repo/issues" do
+  # binding.pry
+  "that's a lotta issues"
+end
+
+get "/hello/:name" do
+  # binding.pry
+  pretty_name = params["name"].capitalize
+  "Hello there, #{pretty_name}!"
+end
+
+post '/hi' do
+  binding.pry
+  "I created a thing"
 end
